873ffa8d3430c0ce125bc5969f7a36f6a5216362
2ailesB <leboudec.lise@gmail.com>
2022-03-29 17:13:55 +0200

    fix/use sklearn preprocess + add kernel as parameter

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -4,6 +4,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.decomposition import PCA, KernelPCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 
 
 class basic_preprocessing(BaseEstimator, TransformerMixin):
@@ -19,10 +20,10 @@ class basic_preprocessing(BaseEstimator, TransformerMixin):
 
 ### TODO : add the parameters of the preprocessing functions from sklearn + in main 
 class pca_preprocessing(BaseEstimator, TransformerMixin):
-    def __init__(self, n_components=None):
+    def __init__(self, n_components=None, kernel='linear'):
         super().__init__()
         self.n_components = n_components
-        self.model = KernelPCA(n_components=n_components)
+        self.model = KernelPCA(n_components=n_components, kernel=kernel)
 
     def fit(self, X, y=None):
         self.model.fit(X)
@@ -46,9 +47,9 @@ class norm_preprocessing(BaseEstimator, TransformerMixin):
         return self.model.transform(X)
 
 class normPCA_preprocessing(BaseEstimator, TransformerMixin):
-    def __init__(self, n_components=None):
+    def __init__(self, n_components=None, kernel='linear'):
         super().__init__()
-        self.model = Pipeline(steps=[("scale", norm_preprocessing()), ("pca", pca_preprocessing(n_components=n_components))])
+        self.model = Pipeline(steps=[("scale", StandardScaler()), ("pca", KernelPCA(n_components=n_components, kernel=kernel))])
 
     def fit(self, X, y=None):
         self.model.fit(X)
@@ -57,3 +58,4 @@ class normPCA_preprocessing(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         # Numerical features to pass down the numerical pipeline
         return self.model.transform(X)
+
