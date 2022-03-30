198ac03f27c2af7e2d46d7bf63d6c8b55c65dfa5
2ailesB <leboudec.lise@gmail.com>
2022-03-30 23:54:03 +0200

    feat/add computing time

--- time.py
+++ time.py
@@ -33,52 +33,24 @@ def main():
     print(f'train data class number:\n {train_data[64].value_counts()}')
     print(f'test data class number:\n {test_data[64].value_counts()}')
 
-    preprocess = preprocessing.normPCA_preprocessing(n_components=10, kernel='rbf')
-    # preprocess = preprocessing.norm_preprocessing()
+    # preprocess = preprocessing.normPCA_preprocessing(n_components=10, kernel='rbf')
+    preprocess = preprocessing.norm_preprocessing()
     preprocess.fit(train_data.iloc[:, 0:64])
     # preprocess=None
     ######################## STEP 2 : apply standard models ########################
     
-    model2test = {'svm' : (classifier.SVMModel(train_data, test_data, preprocess), {"model__kernel": ["poly", "rbf"], "model__C": [0.1, 1, 100]}),
-                    'rf' : (classifier.RFModel(train_data, test_data, preprocess), {"model__n_estimators": [100, 50], "model__criterion": ["gini", "entropy"], "model__max_depth":[None, 10]}),
-                    'logistic regression' : (classifier.LRModel(train_data, test_data, preprocess), {"model__penalty": ['l1', 'l2', 'elasticnet', 'none'], "model__C":[0.1, 1, 10]}), 
+    model2test = {'svm' : (classifier.SVMModel(train_data, test_data, preprocess), {"kernel": "rbf", "C": 100}),
+                    'rf' : (classifier.RFModel(train_data, test_data, preprocess), {"n_estimators": 100, "criterion": "entropy", "max_depth":None}),
+                    'logistic regression' : (classifier.LRModel(train_data, test_data, preprocess), {"penalty": 'l1', "C":0.1}), 
                     'lda': (classifier.lda(train_data, test_data, preprocess), {}), 
                     'qda': (classifier.qda(train_data, test_data, preprocess), {}), 
-                    'sk_MLP': (classifier.sk_NN(train_data, test_data, preprocess), {'model__hidden_layer_sizes':[(10), (15, 15), (32, 16, 8)], 'model__alpha':[0, 0.1], 'model__max_iter':[100], 'model__batch_size':[100]})}
+                    'sk_MLP': (classifier.sk_NN(train_data, test_data, preprocess), {'hidden_layer_sizes':(32, 16, 8), 'alpha':0.1, 'max_iter':100, 'batch_size':100}), 
+                    'multi_rf':(classifier.MultiRFModel(train_data, test_data, preprocess), {})}
 
-    # model2test = {'rf' : (classifier.RFModel(train_data, test_data,  preprocess), {"model__n_estimators": [100, 50], "model__criterion": ["gini", "entropy"], "model__max_depth":[None, 10]})}
     for name, (model, param_grid) in model2test.items():
-        model.grid_search(param_grid, n_fold=2)
-        pred = model.predict()
-        model.save(f'saved_models/{name}')
-        model.visualisation(f'figures/{name}.png')
-        print(f'{name} model after CV grid search has accuracy of {model.score(f1_micro)}')
-
-    # model.load('/home/yunfei/Desktop/ml-for-medical-data/saved_models/SVM')
-    # model.load('saved_models/SVM')
+        t_time, e_time = model.estimate_time(param_grid)
+        print(f'{name} model has training time {t_time} and execution time {e_time}')
     
-    ####################### STEP 3 : apply deep models ########################
-    model = NN.NN_classifier(train_data, test_data, 64, [32, 16, 8], 4)
-    name = 'MLP'
-    lr, epochs, batch_size = 1e-4, 100, 100
-    start_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    writer = SummaryWriter('figures/runs/NN_' + start_time )
-    model.fit(nn.CrossEntropyLoss(), lr, epochs, batch_size, writer)
-    pred = model.predict()
-    model.save(lr, epochs, f'saved_models/{name}', '')
-    # model.visualisation(f'figures/{name}.png')
-    print(f'{name} model after training has accuracy of {model.score(accuracy)}')
-    
-    model = NN.LSTM_classifier2(train_data, test_data, input_size=8, num_layers=4, hidden_size=16, proj_size=4, final_activation=nn.Softmax())
-    name = 'LSTM'
-    lr, epochs, batch_size = 1e-4, 100, 100
-    start_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    writer = SummaryWriter('figures/runs/LSTM_' + start_time )
-    model.fit(nn.CrossEntropyLoss(), lr, epochs, batch_size, writer)
-    pred = model.predict()
-    model.save(lr, epochs, f'saved_models/{name}', '')
-    # model.visualisation(f'figures/{name}.png')
-    print(f'{name} model after training has accuracy of {model.score(accuracy)}')
 
 
 if __name__ == '__main__':
